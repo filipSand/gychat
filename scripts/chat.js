665f3a17d2790651a3bbde542e9a57ed06f04c9e
Filip Sandström <fsa0726@student.skelleftea.se>
2021-01-11 10:59:29 +0100

    Messages now appear correctly and auto-scroll

---
+++ scripts/chat.js
@@ -0,0 +1,10 @@
+//https://stackoverflow.com/questions/11715646/scroll-automatically-to-the-bottom-of-the-page
+// var scrollingElement = (document.scrollingElement || document.body);
+// scrollingElement.scrollTop = scrollingElement.scrollHeight;
+
+window.scrollTo(0, document.body.scrollHeight);
+
+
+
+
+
